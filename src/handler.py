7124968ccfe66590bf2604f3291cae3409f94555
dozie <dozie.steve@gmail.com>
2024-04-01 06:10:37 +0100

    mora first commit

--- src/handler.py
+++ src/handler.py
@@ -1,20 +1,74 @@
+import mimetypes
+import json
+import os
 import time
 import subprocess
 
 import runpod
 import requests
 from requests.adapters import HTTPAdapter, Retry
+from firebase_admin import credentials, initialize_app, storage, firestore
+
+import torch
+from diffusers import PixArtAlphaPipeline
+from runpod.serverless.modules.rp_logger import RunPodLogger
+from runpod.serverless.utils.rp_validator import validate
+
+from src.mora import load_models, Get_image, generate_and_concatenate_videos
+
+logger = RunPodLogger()
 
 LOCAL_URL = "http://127.0.0.1:5000"
 
-cog_session = requests.Session()
-retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
-cog_session.mount('http://', HTTPAdapter(max_retries=retries))
+SERVICE_CERT = json.loads(os.environ["FIREBASE_KEY"])
+# SADTALKER_SERVICE_CERT = json.loads(os.environ["SADTALKER_FIREBASE_KEY"])
+STORAGE_BUCKET = os.environ["STORAGE_BUCKET"]
+
+cred_obj = credentials.Certificate(SERVICE_CERT)
+# sad_cred_obj = credentials.Certificate(SADTALKER_SERVICE_CERT)
+
+default_app = initialize_app(cred_obj, {"storageBucket": STORAGE_BUCKET}, name='mora')
+# sad_app = initialize_app(sad_cred_obj, name='sadtalker')
+
+
+INPUT_SCHEMA = {
+    "prompt": {
+        "type": str,
+        "title": "Prompt",
+        "default": "With the style of van gogh, A young couple dances under the moonlight by the lake.",
+        "required": True,
+        "description": "Prompt for video generation."
+    }
+}
+
+
+def get_extension_from_mime(mime_type):
+    extension = mimetypes.guess_extension(mime_type)
+    return extension
+
+
+def upload_video(filename):
+    destination_blob_name = f'mora/{filename}'
+    bucket = storage.bucket(app=default_app)
+    blob = bucket.blob(destination_blob_name)
+
+    blob.upload_from_filename(filename)
+
+    # Opt : if you want to make public access from the URL
+    blob.make_public()
+
+    logger.info("File uploaded to firebase...")
+    return blob.public_url
+
+
+# cog_session = requests.Session()
+# retries = Retry(total=10, backoff_factor=0.1, status_forcelist=[502, 503, 504])
+# cog_session.mount('http://', HTTPAdapter(max_retries=retries))
 
 
 # ----------------------------- Start API Service ---------------------------- #
 # Call "python -m cog.server.http" in a subprocess to start the API service.
-subprocess.Popen(["python", "-m", "cog.server.http"])
+# subprocess.Popen(["python", "-m", "cog.server.http"])
 
 
 # ---------------------------------------------------------------------------- #
@@ -45,9 +99,14 @@ def run_inference(inference_request):
     '''
     Run inference on a request.
     '''
-    response = cog_session.post(url=f'{LOCAL_URL}/predictions',
-                                json=inference_request, timeout=600)
-    return response.json()
+    prompt = inference_request["prompt"]
+    pipe, base, refiner, n_steps, high_noise_frac = load_models()
+
+    # Example usage
+    # Get_image("A boy", base, refiner, n_steps, high_noise_frac)
+    Get_image(prompt, base, refiner, n_steps, high_noise_frac)
+    output_file = generate_and_concatenate_videos("image.png", pipe, 3)
+    return output_file
 
 
 # ---------------------------------------------------------------------------- #
@@ -58,9 +117,22 @@ def handler(event):
     This is the handler function that will be called by the serverless.
     '''
 
-    json = run_inference({"input": event["input"]})
+    validated_input = validate(event["input"], INPUT_SCHEMA)
 
-    return json["output"]
+    if 'errors' in validated_input:
+        logger.error('Error in input...')
+        return {
+            'errors': validated_input['errors']
+        }
+
+    logger.info('Input validated...')
+
+    valid_input = validated_input['validated_input']
+
+    result = run_inference({"input": valid_input})
+    url = upload_video(result)
+
+    return url
 
 
 if __name__ == "__main__":
---
+++ src/mora.py
@@ -0,0 +1,105 @@
+import time
+import torch
+from diffusers import StableVideoDiffusionPipeline, DiffusionPipeline
+from diffusers.utils import load_image, export_to_video
+from moviepy.editor import VideoFileClip, concatenate_videoclips
+from PIL import Image
+
+
+def load_models():
+    # Load the stable video diffusion pipeline
+    pipe = StableVideoDiffusionPipeline.from_pretrained(
+        "stabilityai/stable-video-diffusion-img2vid-xt", torch_dtype=torch.float16, variant="fp16"
+    )
+    pipe.enable_model_cpu_offload()
+
+    # Load the stable diffusion pipeline for base and refiner
+    base = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-base-1.0", torch_dtype=torch.float16, variant="fp16", use_safetensors=True
+    )
+    base.to("cuda")
+    refiner = DiffusionPipeline.from_pretrained(
+        "stabilityai/stable-diffusion-xl-refiner-1.0",
+        text_encoder_2=base.text_encoder_2,
+        vae=base.vae,
+        torch_dtype=torch.float16,
+        use_safetensors=True,
+        variant="fp16",
+    )
+    refiner.to("cuda")
+
+    # Define how many steps and what % of steps to be run on each expert (80/20)
+    n_steps = 40
+    high_noise_frac = 0.8
+
+    return pipe, base, refiner, n_steps, high_noise_frac
+
+
+def Get_image(prompt, base, refiner, n_steps, high_noise_frac):
+    # Run both experts
+    image = base(
+        prompt=prompt,
+        num_inference_steps=n_steps,
+        denoising_end=high_noise_frac,
+        height=576,
+        width=1024,
+        output_type="latent",
+    ).images
+    image = refiner(
+        prompt=prompt,
+        num_inference_steps=n_steps,
+        denoising_start=high_noise_frac,
+        height=576,
+        width=1024,
+        image=image,
+    ).images[0]
+    image.save('image.png')
+    return image
+
+
+def Get_Last_Frame(video_path, output_image_path):
+    # Load the video file using VideoFileClip
+    with VideoFileClip(video_path) as video:
+        # Get the last frame by going to the last second of the video
+        last_frame = video.get_frame(video.duration - 0.01)  # a fraction before the end
+
+    # Now, we save the last frame as an image using PIL
+    last_frame_image = Image.fromarray(last_frame)
+    last_frame_image.save(output_image_path)
+    return last_frame_image
+
+
+def generate_and_concatenate_videos(initial_image_path, pipe, num_iterations=60):
+    video_paths = []  # To keep track of all generated video paths
+    current_image_path = initial_image_path
+
+    for iteration in range(num_iterations):
+        # Generate frames based on the current image
+        image = Image.open(current_image_path).resize((1024, 576))
+        seed = int(time.time())
+        torch.manual_seed(seed)
+        frames = pipe(image, decode_chunk_size=12, generator=torch.Generator(), motion_bucket_id=127).frames[0]
+
+        # Export frames to video and save the path
+        video_path = f"video_segment_{iteration}.mp4"
+        export_to_video(frames, video_path, fps=25)
+        video_paths.append(video_path)
+
+        # Get the last frame of the current video for the next iteration
+        current_image_path = Get_Last_Frame(video_path, "1.png")
+
+    # Load and concatenate all video segments
+    clips = [VideoFileClip(path) for path in video_paths]
+    final_clip = concatenate_videoclips(clips)
+
+    # Save the final video
+    final_clip.write_videofile("final_output_video.mp4")
+    return "final_output_video.mp4"
+
+
+if __name__ == "__main__":
+    pipe, base, refiner, n_steps, high_noise_frac = load_models()
+
+    # Example usage
+    Get_image("A boy", base, refiner, n_steps, high_noise_frac)
+    generate_and_concatenate_videos("image.png", pipe, 3)
